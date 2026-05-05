7ddec790de73806b6727caa65ddb2ccdd693684b
Chase Helgenlechner <chase@helgenlechner.com>
2026-05-05 16:14:15 +0200

    Initial commit. Demo program working based on what was shown in the website.

---
+++ Demo/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public PersonModel(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Demo/ViewModels/RelayCommandnewold.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Demo.ViewModels
+{
+    public class RelayCommandnewold : ICommand
+    {
+        private readonly Action _execute;
+
+        public RelayCommandnewold(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter) => _execute();
+    }
+}
